ad3694b93616836887cb0296258189fa87348b7a
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2025-09-29 17:18:21 -0500

    update

---
+++ Modulo_6/AE5/server.js
@@ -0,0 +1,28 @@
+//Importaciones
+const express = require('express');
+const paisesFunc= require('./paises.js');  //importo el modulo de paises
+//configuraciones
+const app = express();
+
+app.use(express.json()); //middleware para parsear el body de las peticiones
+
+const port = 8080;
+
+
+//procesos
+app.get('/paises',(req,res)=>{
+    const paises = paisesFunc.getPaises();
+    res.json(paises);
+})
+
+app.post('/paises',(req,res)=>{
+    const nuevoPais = req.body;
+    const paises = paisesFunc.agregarPais(nuevoPais);
+    res.json(paises);
+})
+
+app.delete('/paises/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const paises = paisesFunc.eliminarPais(id);
+    res.json(paises);
+})
